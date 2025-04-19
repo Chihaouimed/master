8418ddd93f9a1b8eca63179a9156b5b3e2b9118c
Chihaouimed <chihaouimohamed@gmail.com>
2025-04-19 02:26:57 +0100

    Delete odoo_project/models directory

--- odoo_project/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from . import pv_installation
-from . import pv_module
-from . import pv_inverter
-from . import configuration_steg
-from . import alarm_management
-from . import reclamation
-from . import fiche_intervention
